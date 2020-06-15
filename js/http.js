8888a9bdddc24e6a4f9dbc1d1609f379daf64bae
LBB <3291282554@qq.com>
2020-06-15 17:26:49 +0800

    1

---
+++ js/http.js
@@ -0,0 +1,13 @@
+(function (w) {
+    var JD = 'http://183.237.67.218:3000';
+    var DZ = {
+        baseURL: JD,
+        newsong: JD + '/personalized/newsong',//新歌
+        highquality: JD + '/top/playlist/highquality',//热门歌单
+        search: JD + '/search',//搜索
+        song_detail: JD + '/song/detail',//歌曲详细
+        song_url: JD + '/song/url',//查询歌曲地址
+        playlist_detail: JD + '/playlist/detail',//歌单详细
+    }
+    w.DZ = DZ;
+})(window)
