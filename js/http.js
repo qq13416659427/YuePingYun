4782c3adba3c67d5c71e1aa6325109bba0609b62
LBB <3291282554@qq.com>
2020-06-16 21:19:10 +0800

    歌词评论过半

--- js/http.js
+++ js/http.js
@@ -8,6 +8,7 @@
         song_detail: JD + '/song/detail',//歌曲详细
         song_url: JD + '/song/url',//查询歌曲地址
         playlist_detail: JD + '/playlist/detail',//歌单详细
+        comment_hot: JD + '/comment/hot',//热门评论
     }
     w.DZ = DZ;
 })(window)
